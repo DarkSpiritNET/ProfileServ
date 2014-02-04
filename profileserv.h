d8a1de8724f80c8b44fbf504719a9a3350ededfc
LucentW <lucent@zebes.info>
2014-02-04 20:22:44 +0100

    First commit
    ProfileServ-3.0.a4-26

---
+++ profileserv.h
@@ -0,0 +1,86 @@
+/* ProfileServ - User Profile Service - NeoStats Addon Module
+** Copyright (c) 2006-2008 Justin Hammond, Mark Hetherington, DeadNotBuried
+**
+**  This program is free software; you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation; either version 2 of the License, or
+**  (at your option) any later version.
+**
+**  This program is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with this program; if not, write to the Free Software
+**  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+**  USA
+**
+** ProfileServ CVS Identification
+** $Id: profileserv.h 26 2008-04-01 06:57:51Z Fish $
+*/
+
+#include MODULECONFIG
+
+/* Defines */
+#define PFS_TYPESIZE		3			/* Type Field Size */
+#define PFS_TEXTSIZE		300			/* Text Field Size */
+#define PFS_TYPENICKSIZE	PFS_TYPESIZE+MAXNICK	/* Type and Nick Key Field Size */
+
+/* Variables And Structs */
+Bot *pfs_bot;
+
+struct ProfileServ {
+	int exclusions;
+	int enable;
+	int enableprofilechan;
+	char profilechan[MAXCHANLEN];
+	int expiredays;
+} ProfileServ;
+
+typedef struct ProfileUser {
+	char nick[MAXNICK];
+	int lastseen;
+} ProfileUser;
+
+typedef struct ProfileData {
+	char typenick[PFS_TYPENICKSIZE];
+	char typedata[PFS_TEXTSIZE];
+} ProfileData;
+
+/* ProfileServ Module Help - ProfileServ_help.c */
+extern const char *pfs_help_set_exclusions[];
+extern const char *pfs_help_set_enable[];
+extern const char *pfs_help_set_enableprofilechan[];
+extern const char *pfs_help_set_profilechanname[];
+extern const char *pfs_help_set_expiredays[];
+extern const char *pfs_help_profile[];
+extern const char *pfs_help_realname[];
+extern const char *pfs_help_birthday[];
+extern const char *pfs_help_age[];
+extern const char *pfs_help_gender[];
+extern const char *pfs_help_maritalstatus[];
+extern const char *pfs_help_location[];
+extern const char *pfs_help_hobbies[];
+extern const char *pfs_help_general[];
+extern const char *pfs_help_del[];
+
+/* profileserv.c */
+int pfs_set_exclusions( const CmdParams *cmdparams, SET_REASON reason );
+int pfs_set_enablechan (const CmdParams *cmdparams, SET_REASON reason);
+int pfs_set_profilechan (const CmdParams *cmdparams, SET_REASON reason);
+void profile_report( const CmdParams *cmdparams, const char *fmt, ... );
+int pfs_cmd_profile (const CmdParams *cmdparams);
+int pfs_cmd_realname (const CmdParams *cmdparams);
+int pfs_cmd_birthday (const CmdParams *cmdparams);
+int pfs_cmd_age (const CmdParams *cmdparams);
+int pfs_cmd_gender (const CmdParams *cmdparams);
+int pfs_cmd_maritalstatus (const CmdParams *cmdparams);
+int pfs_cmd_location (const CmdParams *cmdparams);
+int pfs_cmd_hobbies (const CmdParams *cmdparams);
+int pfs_cmd_general (const CmdParams *cmdparams);
+int pfs_cmd_del (const CmdParams *cmdparams);
+void pfs_delete_profile ( const CmdParams *cmdparams, ProfileUser *pu );
+int pfs_expire_profiles(void*);
+int expireoldprofiles(void *data, int size);
+int pfs_UmodeUser (const CmdParams *cmdparams);
---
+++ profileserv_help.c
@@ -0,0 +1,139 @@
+/* ProfileServ - User Profile Service - NeoStats Addon Module
+** Copyright (c) 2006-2008 Justin Hammond, Mark Hetherington, DeadNotBuried
+**
+**  This program is free software; you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License as published by
+**  the Free Software Foundation; either version 2 of the License, or
+**  (at your option) any later version.
+**
+**  This program is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with this program; if not, write to the Free Software
+**  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+**  USA
+**
+** ProfileServ CVS Identification
+** $Id: profileserv_help.c 26 2008-04-01 06:57:51Z Fish $
+*/
+
+#include "neostats.h"    /* Required for bot support */
+#include "profileserv.h"
+
+const char *pfs_help_set_exclusions[] = {
+	"\2EXCLUSIONS <ON|OFF>\2",
+	"Use global exclusion list in addition to local exclusion list",
+	NULL
+};
+
+const char *pfs_help_set_enable[] = {
+	"\2ENABLE <ON|OFF>\2",
+	"Enable or Disable ProfileServ usage via private message",
+	NULL
+};
+
+const char *pfs_help_set_enableprofilechan[] = {
+	"\2ENABLEPROFILECHAN <ON|OFF>\2",
+	"Enable ProfileServ to join the Profile Channel and accept commands in channel",
+	NULL
+};
+
+const char *pfs_help_set_profilechanname[] = {
+	"\2PROFILECHANNAME <#Channel>\2",
+	"Set Channel ProfileServ accepts commands from in channel",
+	NULL
+};
+
+const char *pfs_help_set_expiredays[] = {
+	"\2EXPIREDAYS <#>\2",
+	"Sets the number of days after which profile data is",
+	"removed, if the nickname has not been seen identified.",
+	"Set to 0 to disable Profile expiry.",
+	NULL
+};
+
+const char *pfs_help_profile[] = {
+	"View Profile - Syntax: \2PROFILE <nick>\2",
+	"Syntax: \2PROFILE <nick>\2",
+	"",
+	"Displays infomation stored in ProfileServ for the entered Nickname.",
+	NULL
+};
+
+const char *pfs_help_realname[] = {
+	"Add Real Name to Profile - Syntax: \2REALNAME <real name>\2",
+	"Syntax: \2REALNAME <real name>\2",
+	"",
+	"Add your Real Name to ProfileServ for your current Nick.",
+	NULL
+};
+
+const char *pfs_help_birthday[] = {
+	"Add birthday to Profile - Syntax: \2BIRTHDAY <birthday>\2",
+	"Syntax: \2BIRTHDAY <birthday>\2",
+	"",
+	"Add your birthday to ProfileServ for your current Nick.",
+	NULL
+};
+
+const char *pfs_help_age[] = {
+	"Add Age to Profile - Syntax: \2AGE <age>\2",
+	"Syntax: \2AGE <age>\2",
+	"",
+	"Add your age (in years) to ProfileServ for your current Nick.",
+	NULL
+};
+
+const char *pfs_help_gender[] = {
+	"Add Gender to Profile - Syntax: \2GENDER <gender>\2",
+	"Syntax: \2GENDER <gender>\2",
+	"",
+	"Add your Gender to ProfileServ for your current Nick.",
+	NULL
+};
+
+const char *pfs_help_maritalstatus[] = {
+	"Add Marital Status to Profile - Syntax: \2MARITAL <marital status>\2",
+	"Syntax: \2MARITAL <marital status>\2",
+	"",
+	"Add your Marital Status to ProfileServ for your current Nick.",
+	NULL
+};
+
+const char *pfs_help_location[] = {
+	"Add Location to Profile - Syntax: \2LOCATION <Country / State / Province / City / Suburb>\2",
+	"Syntax: \2LOCATION <Country / State / Province / City / Suburb>\2",
+	"",
+	"Add your Location to ProfileServ for your current Nick.",
+	NULL
+};
+
+const char *pfs_help_hobbies[] = {
+	"Add Hobbies to Profile - Syntax: \2HOBBIES <Hobby List>\2",
+	"Syntax: \2HOBBIES <Hobby List>\2",
+	"",
+	"Add your List of Hobbies to ProfileServ for your current Nick.",
+	NULL
+};
+
+const char *pfs_help_general[] = {
+	"Add General information to Profile - Syntax: \2GENERAL <Line 1-5> <text>\2",
+	"Syntax: \2GENERAL <Line 1-5> <text>\2",
+	"",
+	"Add General information to ProfileServ for your current Nick.",
+	"Up to 5 lines of general Information may be entered.",
+	"# MUST be a number from 1 to 5, being the line number for the General information.",
+	NULL
+};
+
+const char *pfs_help_del[] = {
+	"Delete a Profile - Syntax: \2DEL <nick>\2",
+	"Syntax: \2DEL <nick>\2",
+	"",
+	"Allows you to delete your profile.",
+	"You must enter the nickname to remove the profile.",
+	NULL
+};
